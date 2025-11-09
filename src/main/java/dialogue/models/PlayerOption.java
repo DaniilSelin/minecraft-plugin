b8987a57588b58c4322a592d7a234d5dc7f27568
DaniilSelin <dan.selin2004@mail.ru>
2025-11-09 22:37:19 +0700

    ку

---
+++ src/main/java/dialogue/models/PlayerOption.java
@@ -0,0 +1,28 @@
+package dialogue.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PlayerOption {
+    @JsonProperty("id")
+    public int id;                   // id опции
+
+    @JsonProperty("text")
+    public String text;                 // текст варианта, что видит игрок
+
+    @JsonProperty("next_stage_id")
+    public int nextStageId;          // куда ведёт выбор
+
+    @JsonProperty("next_dialogue_id")
+    public int nextDialogueId;       // переход в другой диалог (неюзабельно)
+
+    @JsonProperty("conditions")
+    public Map<String, Object> conditions; // условия видимости (ключи: "level", "flag", "item" и т.п.)
+
+    @JsonProperty("effects")
+    public Map<String, Object> effects;    // эффекты при выборе (ставит флаги, даёт предметы и т.п.)
+}
